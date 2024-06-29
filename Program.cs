3469489dd1e4695e01045aad2b29ce31fde8b0c4
Hemavathi987 <136674928+Hemavathi987@users.noreply.github.com>
2024-06-29 12:53:34 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+namespace Access4
+{
+    internal class Complex
+    {
+        internal int real;
+        internal int img;
+
+        public void setData(int r, int i)
+        {
+            real = r;
+            img = i;
+        }
+
+        public void displayData()
+        {
+            Console.WriteLine("Real "+ real);
+            Console.WriteLine("Imaginary "+ img);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            Complex c = new Complex();
+            c.setData(2, 1);
+            c.displayData();
+        }
+    }
+}
+
